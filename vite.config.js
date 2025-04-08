988c6f9b3bba359c815e3a34ce21d7908bb8e606
jai <jai@ouverture.ai>
2025-04-08 12:48:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
-  },
   resolve: {
     alias: {
       'jwt-decode': 'jwt-decode/dist/jwt-decode.esm.js'
     }
   },
   plugins: [react()],
-})
+});
