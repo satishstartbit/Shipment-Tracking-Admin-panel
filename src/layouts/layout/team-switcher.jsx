aaa6eefbeab2e63ee1b042bd36d791e7d3d98be4
Satish choudhary <satishchoudhary@startbitsolutions.com>
2025-04-09 16:03:07 +0530

    code review

--- src/layouts/layout/team-switcher.jsx
+++ src/layouts/layout/team-switcher.jsx
@@ -1,29 +1,28 @@
 import {
   DropdownMenu,
   DropdownMenuTrigger,
-} from "../../components/ui/dropdown-menu"; // Importing the DropdownMenu and DropdownMenuTrigger components from your custom UI library
+} from "../../components/ui/dropdown-menu";
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "../../components/ui/sidebar"; // Importing SidebarMenu related components from your UI library
-import logo from "../../assets/img/logo.png"; // Importing the logo image
+} from "../../components/ui/sidebar";
+import logo from "../../assets/img/logo.png";
 
-// TeamSwitcher component which will allow switching teams or displaying related actions
 export function TeamSwitcher() {
+
   return (
-    <SidebarMenu> {/* Wrapper for the sidebar menu */}
-      <SidebarMenuItem> {/* A single item in the sidebar menu */}
-        <DropdownMenu> {/* The dropdown menu container */}
-          <DropdownMenuTrigger asChild> {/* Trigger element that opens the dropdown menu */}
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              {/* This button contains the logo and is used as the dropdown menu trigger */}
-              <div className="flex rounded-[20px] overflow-hidden justify-center">
+              <div className="flex rounded-[20px] overflow-hidden  justify-center">
                 <img
-                  src={logo} // The logo image that will be displayed in the menu
+                  src={logo}
                   className="w-40 h-30 object-contain rounded-[20px] p-2"
                 />
               </div>
